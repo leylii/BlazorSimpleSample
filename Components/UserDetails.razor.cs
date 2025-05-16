1e67961746be9c60c3a36c1a7ba2ccff8c410e73
leilamo <leilam@aiutrix.com>
2025-05-16 17:45:28 +0200

    Commit 4 transfer to GitHub

---
+++ Components/UserDetails.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorSimpleSample.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorSimpleSample.Components
+{
+    public partial class UserDetails : ComponentBase
+    {
+        [Parameter] public required User SelectedUser { get; set; }
+        [Parameter] public EventCallback OnClose { get; set; }
+
+        private async Task CloseModal()
+        {
+            await OnClose.InvokeAsync();
+        }
+    }   
+}
